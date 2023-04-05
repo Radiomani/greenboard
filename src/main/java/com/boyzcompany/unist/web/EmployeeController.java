85c4653d29add8f34c9ca928f8e35de6b8f5572a
Vyacheslav Kim <kimslava@unist.ac.kr>
2023-04-05 16:19:38 +0900

    Part 2 works

--- src/main/java/com/boyzcompany/unist/web/EmployeeController.java
+++ src/main/java/com/boyzcompany/unist/web/EmployeeController.java
@@ -1,26 +1,22 @@
 package com.boyzcompany.unist.web;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
-import org.bson.codecs.CollectibleCodec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.boyzcompany.unist.dal.EmployeeRepository;
 import com.boyzcompany.unist.model.Employee;
-import com.boyzcompany.unist.repository.EmployeeRepository;
 
 @RestController
 public class EmployeeController {
@@ -29,29 +25,33 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @GetMapping("/employees")
-    public Collection<Employee> getAllEmployees() {
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable String id) {
-        return employeeRepository.find(id);
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            return employee.get();
+        } else
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return employee;
-        // return employeeRepository.add(employee);
+    @PostMapping(value = "/employees", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addEmployee(@RequestBody Employee employee) {
+        employeeRepository.save(employee);
     }
 
     @PutMapping("/employees/{id}")
-    public Employee replaceEmployee(@RequestParam(name = "id", required = true) String id,
-            @RequestBody Employee employee) {
-        return employeeRepository.replace(id, employee);
+    public void replaceEmployee(@PathVariable String id, @RequestBody Employee employee) {
+        employeeRepository.deleteById(id);
+        employee.setEmployeeId(id);
+        employeeRepository.save(employee);
     }
 
     @DeleteMapping("/employees/{id}")
-    public Employee deleteEmployee(@RequestParam(name = "id", required = true) String id) {
-        return employeeRepository.delete(id);
+    public void deleteEmployee(@PathVariable String id) {
+        employeeRepository.deleteById(id);
     }
 }

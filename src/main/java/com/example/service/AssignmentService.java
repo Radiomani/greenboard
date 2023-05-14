f00d14aa0088c16fc720b86aef52deb080a68c18
Vyacheslav Kim <kimslava@unist.ac.kr>
2023-05-15 03:52:36 +0900

    waht else

--- src/main/java/com/example/service/AssignmentService.java
+++ src/main/java/com/example/service/AssignmentService.java
@@ -8,16 +8,8 @@ import com.mongodb.client.result.UpdateResult;
 
 public interface AssignmentService {
 
+    String getAssingmentByCourseID(String course_id);
     String getAssingmentByStudentID(String student_id);
-    String getAssingmentByCourseID(String student_id);
-    List<Assignment> getAssignmentByCourseIDandCourseID();
-
-    public Optional<Assignment> getAssignmentById(int id);
-
-    public Optional<Assignment> getAssignmentById(String id);
-
-    public UpdateResult updateAssignmentById(int id, Assignment updated_assignment);
-
-    public int getMaxId();
+    List<Assignment> getAssignmentByCourseIDandStudentID(String course_id, String student_id);
 
 }

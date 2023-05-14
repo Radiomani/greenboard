18e87a43fbccb233c845dbecee827550a5986b57
adayzhan <batthurt2@gmail.com>
2023-05-15 02:04:36 +0900

    blya 2 chasa nochi ebat AHAHAHAHH

--- src/main/java/com/example/repositories/StudentRepository.java
+++ src/main/java/com/example/repositories/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.model.Student;
 
 
-@Repository
-public interface StudentRepository extends MongoRepository<Student, String> {
-    Student findById();
+public interface StudentRepository {
+    Student getStudentByID();
 }
+

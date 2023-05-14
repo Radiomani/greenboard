2c2926dd5ddf43fcbd79d39cc54a19d10a8b9f3b
Vyacheslav Kim <kimslava@unist.ac.kr>
2023-05-15 04:11:58 +0900

    ass

--- src/main/java/com/example/service/AssignmentService.java
+++ src/main/java/com/example/service/AssignmentService.java
@@ -9,5 +9,5 @@ public interface AssignmentService {
     String getAssingmentsByCourseID(String course_id);
     String getAssingmentsByStudentID(String student_id);
     List<Assignment> getAssignmentsByCourseIDandStudentID(String course_id, String student_id);
-    
+    Assignment getAssignmentByCourseIDandStudentIDandByAssingmentID(String course_id, String student_id, String assingment_id);
 }

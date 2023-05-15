7ec5dc00cd28255eda26ccb9ca86cec2c5ebdbfd
adyzhan <batthurt2@gmail.com>
2023-05-15 17:39:17 +0600

    pisya

--- src/main/java/com/example/repositories/StudentRepository.java
+++ src/main/java/com/example/repositories/StudentRepository.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.example.model.Student;
 
-
+@Repository
 public interface StudentRepository {
-    Student getStudentByID();
+    Student getStudentByID(String student_id);
 }
 
